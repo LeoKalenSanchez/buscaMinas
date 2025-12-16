dc888398934cf1d8da5a8101413ccdd26fdbf2a7
LeoKalenSanchez <leokalensanchez06@gmail.com>
2025-12-16 16:51:25 +0100

    second commit

--- src/busca_minas/Buscaminas.java
+++ src/busca_minas/Buscaminas.java
@@ -1,10 +1,12 @@
 package busca_minas;
 
 public class Buscaminas {
-
+	/**
+	 * Documentacion buscaminas
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
